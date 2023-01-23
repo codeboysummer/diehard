968390f04aa36de01c6246b340e742bb08fd84c2
codeboysummer <aw.wahid13@gmail.com>
2023-01-23 13:35:57 -0500

    'used the precedent next template'

---
+++ components/nav/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Menu } from "lucide-react";
+const Navbar = () => {
+  return (
+    <div className="h-screen w-screen bg-gray-300">
+      <div className="flex h-16 w-full items-center pl-20  ">
+<div className=" p-3 bg-white rounded-lg"><Menu/></div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
